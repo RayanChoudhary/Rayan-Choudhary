0ab68e3a458bbff2e42f8d0f922299ab2942ef42
Muhammad Rayan <160248278+RayanChoudhary@users.noreply.github.com>
2024-03-18 14:34:34 -0700

    Add files via upload
    Exited to share my latest project: a calculator built with typescript  I'm thrilled to show my newly developed calculator , crafted entirely using Typescript This project was an amazing opportunity to dive deep into Typescript powerful features and harness its capability to create a user friendly efficient  tool by leveraging typescripts strong programming concepts I've not only built afunctional calculator but also gained Valuble insights into written clean maintainable code check and let me know your thoughts I'm always open to feedback and eager to continue learning and growing as a developer.

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstnumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one the operators to perform  operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//CONDITIONAL STATEMENT
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstnumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondNumber);
+}
+else {
+    "please select correct operator";
+}
